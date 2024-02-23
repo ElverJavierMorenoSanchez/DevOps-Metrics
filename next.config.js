045e461e8bb6b81a8bf928c7513b1eedfcfb0abc
Elver Javier <ejmoreno@gmail.com>
2024-02-23 11:51:21 -0500

    test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  basePath: "/",
-  assetPrefix: "/",
+  basePath: "",
+  assetPrefix: "",
 };
 
 module.exports = nextConfig;
